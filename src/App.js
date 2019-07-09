29957bf45d5d9dc4acce2cbe7d082495e94146eb
github username <github email address>
2019-07-09 15:22:33 -0400

    creating hand functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CaptionContainer from './CaptionContainer.js';
 import MemeContainer from './MemeContainer.js';
 import ActiveMemeCaption from './ActiveMemeCaption.js'
+import Hand from './Hand.js'
 import './App.css';
 
 class App extends React.Component {
@@ -10,6 +11,7 @@ class App extends React.Component {
     captions: [],
     memes: [],
     activeMeme: null,
+    activeCaptions: [],
     activeCaption: null
   }
 
@@ -36,7 +38,14 @@ class App extends React.Component {
       activeMeme: meme
     })
   }
+
   handleCaptionClick = (caption) => {
+    this.setState({
+      activeCaptions: [...this.state.activeCaptions, caption]
+    })
+  }
+
+  handleActiveCaptionClick = (caption) => {
     this.setState({
       activeCaption: caption
     })
@@ -49,6 +58,7 @@ class App extends React.Component {
       <CaptionContainer captions={this.state.captions} handleCaptionClick={this.handleCaptionClick}/>
       <MemeContainer memes={this.state.memes} handleMemeClick={this.handleMemeClick}/>
       <ActiveMemeCaption caption={this.state.activeCaption} meme={this.state.activeMeme}/>
+      <Hand captions={this.state.activeCaptions} handleCaptionClick={this.handleActiveCaptionClick}/>
     </div>
     )
   }
