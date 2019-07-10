479f15a697a10d0a8ab975b22ce9ec159f01f8f1
github username <github email address>
2019-07-10 12:32:13 -0400

    Meme and caption buttons + some styling

--- src/ActiveMemeCaption.js
+++ src/ActiveMemeCaption.js
@@ -7,7 +7,7 @@ export default class ActiveMemeCaption extends React.Component {
   render(){
     return (
       <div className="play">
-        {this.props.meme ? <img src={this.props.meme.url}/> : ""}
+        {this.props.meme ? <img className= "activeMeme" src={this.props.meme.url}/> : ""}
         {this.props.caption ? this.props.caption.title : ""}
       </div>
     )
--- src/CaptionContainer.js
+++ src/CaptionContainer.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import Caption from './Caption.js'
+// import Caption from './Caption.js'
 
 export default class CaptionContainer extends React.Component {
 
+
+  handleClick = () => {
+    this.props.handleCaptionClick(this.props.captions[Math.floor(Math.random()*this.props.captions.length)])
+  }
+
   render(){
     return (
-      <div>
-        <ul>
-          {this.props.captions.map((caption) => {
-            return <Caption key={caption.id} caption={caption} handleCaptionClick={this.props.handleCaptionClick}/>
-          })}
-        </ul>
+      <div className="captions">
+        <button onClick={this.handleClick}>Caption</button>
       </div>
+      // <div>
+      //   <ul>
+      //     {this.props.captions.map((caption) => {
+      //       return <Caption key={caption.id} caption={caption} handleCaptionClick={this.props.handleCaptionClick}/>
+      //     })}
+      //   </ul>
+      // </div>
     )
   }
 
