de9f2c15058a55db3ffe5dd852a81eb1d485996f
github username <github email address>
2019-07-15 11:15:23 -0400

    Caption handling

--- src/CaptionContainer.js
+++ src/CaptionContainer.js
@@ -3,24 +3,19 @@ import React from 'react';
 
 export default class CaptionContainer extends React.Component {
 
-  state = {
-    clicks: 0
-  }
 
 
 
 
   handleClick = () => {
     this.props.handleCaptionClick(this.props.captions[Math.floor(Math.random()*this.props.captions.length)]);
-    this.setState(prevState => ({
-      clicks: prevState.clicks + 1
-    }));
+    this.props.handleClickCounter()
   }
 
   render(){
     return (
       <div className="captions">
-        {this.state.clicks < 5 ?
+        {this.props.clicks < 5 ?
           <button onClick={this.handleClick}>Caption</button> :
           <button disabled>Caption</button>}
       </div>
