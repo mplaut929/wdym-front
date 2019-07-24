bc0a347989a00989b01b7d15241b31075cf9abab
github username <github email address>
2019-07-23 20:30:53 -0400

    saved meme page

--- src/SavedMemes.js
+++ src/SavedMemes.js
@@ -4,8 +4,35 @@ import React from 'react';
 
 export default class SavedMemes extends React.Component {
 
+  state = {
+    savedMemes: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/meme_captions')
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        savedMemes: data
+      })
+    })
+  }
+
 
   render(){
-    return "hi"
+    console.log(this.state)
+    return (
+      <div>
+        {this.state.savedMemes.map((savedMeme) => {
+          return (
+            <div>
+              <img src={savedMeme.meme.url}/>
+              {savedMeme.caption.title}
+            </div>
+          )
+        })}
+      </div>
+
+    )
   }
 }
